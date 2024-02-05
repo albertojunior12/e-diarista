eea400df1e830c14870589035e569aca55ad81a0
Alberto Macedo <alberto_junior-400@hotmail.com>
2024-02-05 02:03:14 -0300

    Configuração do MaterialUI, Storybook e criado primeiro component com Storybook

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../UI/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ThemeProvider } from '@mui/material';
+import '@styles/globals.css';
+import type { AppProps } from 'next/app';
+import theme from 'UI/themes/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
